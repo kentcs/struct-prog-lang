cf6b61a6c57ce26ac346309591f9b9dc1e8d93e1
Zyaire Bush <zybush@gmail.com>
2026-01-28 06:12:33 +0000

    commited

--- topic-01-simple-expressions/tokenizer.py
+++ topic-01-simple-expressions/tokenizer.py
@@ -1,1 +1,36 @@
-print("hello")
+import re
+
+#p = re.compile("ab*")
+
+#if p.match("a") :
+  #  print("match")
+  #  else
+   # print("not match")
+
+# code for chuds
+
+patterns = [
+    (r"\s+", "whitespace"),
+    (r"\d+", "number"),
+    (r"\+", "+"),
+     (r"\-", "-"),
+      (r"\/", "/"),
+       (r"\*", "*"),
+        (r".", "error")
+]
+
+patterns = [(re.compile(p), tag) for p,tag in patterns]
+
+def tokenize(characters): 
+    "Tokenize a string using the patterns above twin"
+    tokens = []
+    position = 0 
+    line = 1
+    column = 1
+
+    while position < len(characters): 
+        for pattern, tag in patterns:
+            match = pattern.match(characters, position)
+            if match:
+             break
+        assert match is not None
